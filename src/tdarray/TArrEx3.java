84a8f2990444cc80b24dfc5eb551daa913ecd12e
su080915 <tbr335@naver.com>
2025-03-24 16:22:26 +0900

    2 d array

--- src/tdarray/TArrEx3.java
+++ src/tdarray/TArrEx3.java
@@ -1,7 +1,40 @@
 package tdarray;
 
+import java.util.Scanner;
+
 public class TArrEx3 {
     public static void main(String[] args) {
+        String[][] arr = new String[10][2];
 
+        int count = 0;
+
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("1. 상품 등록 | 2. 상품 목록 | 3. 종료");
+            System.out.print("메뉴를 선택하세요: ");
+            int choice = sc.nextInt();
+
+            if (choice == 1) {
+                System.out.print("이름을 입력하세요: ");
+                String name = sc.next();
+
+                System.out.print("가격을 입력하세요: ");
+                String price = sc.next();
+
+                arr[count][0] = name;
+                arr[count][1] = price;
+
+                count++;
+            } else if (choice == 2) {
+                for (int i = 0; i < count; i++) {
+//                    if (arr[i][0] == null) continue;
+//                    if (arr[i][1] == null) continue;
+                    System.out.println(arr[i][0] + ": " + arr[i][1] + "원");
+                }
+            } else if (choice == 3) {
+                break;
+            }
+        }
     }
 }
