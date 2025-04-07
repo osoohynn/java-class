15ad23662ca9866b67dffd21760d430116f215ba
su080915 <tbr335@naver.com>
2025-04-07 13:51:03 +0900

    class

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,24 @@
+import classes.Book;
+import classes.ClassEx3;
 import classes.Student;
 
 public class Main {
     public static void main(String[] args) {
-        Student student = new Student("권수현", 2401, 2);
-        student.introduce();
+//        Student student = new Student("권수현", 2401, 2);
+//        student.introduce();
+
+//        Book book = new Book("흰", "한강", 15000);
+//        book.info();
+
+        ClassEx3 r1 = new ClassEx3("인셉션", "인생은 무한루프");
+        ClassEx3 r2 = new ClassEx3("어바웃 타임", "인생 시간 영화");
+
+        ClassEx3[] r = new ClassEx3[2];
+        r[0] = r1;
+        r[1] = r2;
+
+        for (ClassEx3 c : r) {
+            System.out.println("영화제목: " + c.getTitle() + ", 리뷰: " + c.getReview());
+        }
     }
 }
--- src/classes/ClassEx.java
+++ src/classes/ClassEx.java
@@ -1,30 +1,41 @@
 package classes;
 
 class Student2 {
-    String name;
-    int age;
-    int grade;
+    private String name;
+    private int age;
+    private int grade;
+
+    public Student2(String name, int age, int grade) {
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+    }
+
+    public String getName() { return this.name; }
+    public int getAge() { return this.age; }
+    public int getGrade() { return this.grade; }
+
+    public void setName(String name) { this.name = name; }
+    public void setAge(int age) { this.age = age; }
+    public void setGrade(int grade) { this.grade = grade; }
+
+    public void showData() {
+        System.out.println("이름: " + this.name + ", 나이: " + this.age + ", 성적: " + this.grade);
+    }
+
+    public void show() {
+        System.out.println("제 이름은 " + this.name + "이고 나이는 " + this.age + "살이고 성적은 " + this.grade + "점 입니다.");
+    }
 }
 
 public class ClassEx {
     public static void main(String[] args) {
-        Student2 s1 = new Student2();
-        Student2 s2 = new Student2();
-
-        s1.name = "김하나";
-        s1.age = 10;
-        s1.grade = 20;
-
-        s2.name = "이지호";
-        s2.age = 20;
-        s2.grade = 95;
-
         Student2[] s = new Student2[2];
-        s[0] = s1;
-        s[1] = s2;
+        s[0] = new Student2("김하나", 18, 20);
+        s[1] = new Student2("이지호", 18, 90);
 
         for (Student2 student : s) {
-            System.out.println("제 이름은 " + student.name + "이고 나이는 " + student.age + "살이고 성적은 " + student.grade + "입니다.");
+            student.show();
         }
     }
 }
