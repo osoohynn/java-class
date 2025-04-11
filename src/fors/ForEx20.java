cca037e32b9c3089ee80865e897b02eceac1fb9d
su080915 <tbr335@naver.com>
2025-04-11 14:08:09 +0900

    programmers

--- src/fors/ForEx20.java
+++ src/fors/ForEx20.java
@@ -4,28 +4,24 @@ import java.util.Arrays;
 
 class ForEx20 {
     public int[] solution(int[] arr, int[][] queries) {
-        int[] answer = new int[queries.length];
+        int[] answer = new int[arr.length];
 
         for (int i = 0; i < queries.length; i++) {
-            int min_max = 1000000;
-            boolean chk = false;
             for (int j = queries[i][0]; j <= queries[i][1]; j++) {
-                if (arr[j] > queries[i][2] && arr[j] < min_max) {
-                    min_max = arr[j];
-                    chk = true;
+                if (j % queries[i][2] == 0) {
+                    arr[j] += 1;
                 }
             }
-            if (!chk) answer[i] = -1;
-            else answer[i] = min_max;
         }
 
-        return answer;
+        return arr;
     }
 
     public static void main(String[] args) {
         ForEx20 forEx20 = new ForEx20();
         int[] arr = {0, 1, 2, 4, 3};
-        int[][] queries = {{0, 4, 2},{0, 3, 2},{0, 2, 2}};
+        int[][] queries = {{0, 4, 1},{0, 3, 2},{0, 3, 3}};
+//        int[][] queries = {{0, 4, 1},{0, 3, 2}};
         System.out.println(Arrays.toString(forEx20.solution(arr, queries)));
     }
 }
