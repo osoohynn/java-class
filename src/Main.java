6c3a85152398b2852b963a6b413b63033e3e1703
su080915 <tbr335@naver.com>
2025-05-26 20:00:17 +0900

    exception

--- src/Main.java
+++ src/Main.java
@@ -10,24 +10,29 @@ public class Main {
 //        Book book = new Book("흰", "한강", 15000);
 //        book.info();
 
-        byte _e = (byte) 130;
-        System.out.println(_e);
+//        byte _e = (byte) 130;
+//        System.out.println(_e);
+//
+//        byte a = 3;
+//        byte b = 5;
+//        int c_ = a + b;
+//        System.out.println(a + b);
+//        System.out.println(c_);
+//
+//        ClassEx3 r1 = new ClassEx3("인셉션", "인생은 무한루프");
+//        ClassEx3 r2 = new ClassEx3("어바웃 타임", "인생 시간 영화");
+//
+//        ClassEx3[] r = new ClassEx3[2];
+//        r[0] = r1;
+//        r[1] = r2;
+//
+//        for (ClassEx3 c : r) {
+//            System.out.println("영화제목: " + c.getTitle() + ", 리뷰: " + c.getReview());
+//        }
 
-        byte a = 3;
-        byte b = 5;
-        int c_ = a + b;
-        System.out.println(a + b);
-        System.out.println(c_);
-
-        ClassEx3 r1 = new ClassEx3("인셉션", "인생은 무한루프");
-        ClassEx3 r2 = new ClassEx3("어바웃 타임", "인생 시간 영화");
-
-        ClassEx3[] r = new ClassEx3[2];
-        r[0] = r1;
-        r[1] = r2;
-
-        for (ClassEx3 c : r) {
-            System.out.println("영화제목: " + c.getTitle() + ", 리뷰: " + c.getReview());
-        }
+        int a = 5;
+        int b = 2;
+        float d = a / b;
+        System.out.println("결과: " + d);
     }
 }
